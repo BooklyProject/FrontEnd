e1701c84dbc12075e8d3f16b2b37303e79e71ef1
Emanuele Conforti <manu.conforti@gmail.com>
2023-01-21 13:22:47 +0100

    routing funzionante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { SchedaUtenteComponent } from './scheda-utente/scheda-utente.component';
+import { SchedaEventiComponent } from './scheda-eventi/scheda-eventi.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'catalogo', pathMatch: 'full'},
   {path: 'catalogo', component: CatalogoComponent},
-  {path: 'eventi', component: SchedaUtenteComponent},
+  {path: 'eventi', component: SchedaEventiComponent},
   {path: 'utente', component: SchedaUtenteComponent}
 ];
 
