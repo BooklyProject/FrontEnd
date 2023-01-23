00483caff9d8b3a5a08a55f32a08d14b734f441a
Emanuele Conforti <manu.conforti@gmail.com>
2023-01-23 16:37:04 +0100

    modifiche

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CatalogoComponent } from './catalogo/catalogo.component';
 import { SchedaUtenteComponent } from './scheda-utente/scheda-utente.component';
 import { SchedaEventiComponent } from './scheda-eventi/scheda-eventi.component';
+import { SchedaRaccolteComponent } from './scheda-raccolte/scheda-raccolte.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'catalogo', pathMatch: 'full'},
   {path: 'catalogo', component: CatalogoComponent},
   {path: 'eventi', component: SchedaEventiComponent},
-  {path: 'utente', component: SchedaUtenteComponent}
+  {path: 'utente', component: SchedaUtenteComponent},
+  {path: 'raccolte', component: SchedaRaccolteComponent}
+
 ];
 
 @NgModule({
